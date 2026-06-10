d87d7ee972fd6af998786d3ed110f0579f89c518
misaka <zelda.me@outlook.com>
2026-06-10 10:50:41 +0800

    7

--- src/config.ts
+++ src/config.ts
@@ -61,7 +61,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "CC",
 	bio: "嘻嘻.",
 	links: [
